bbcb12c0daec890dd90f24507b89fcd829ae835c
Sophia Deng <hustdengbaoqin@gmail.com>
2025-05-06 14:51:03 -0400

    display to right side

--- styler_field.py
+++ styler_field.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-# Define your table manually
 html = """
-<table style="border-collapse: collapse;">
-  <tr>
-    <td style="background-color: lightcoral; padding: 8px; width: 100px;"></td>
-    <td style="padding: 8px;">Red</td>
-  </tr>
-  <tr>
-    <td style="background-color: khaki; padding: 8px;"></td>
-    <td style="padding: 8px;">Yellow</td>
-  </tr>
-</table>
+<div style="display: flex; justify-content: flex-end;">
+  <table style="border-collapse: collapse;">
+    <tr>
+      <td style="background-color: lightcoral; padding: 8px; width: 100px;"></td>
+      <td style="padding: 8px;">Red</td>
+    </tr>
+    <tr>
+      <td style="background-color: khaki; padding: 8px;"></td>
+      <td style="padding: 8px;">Yellow</td>
+    </tr>
+  </table>
+</div>
 """
 
-# Display in Streamlit
 st.markdown(html, unsafe_allow_html=True)
